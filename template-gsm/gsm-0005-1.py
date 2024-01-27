0af66f3d2846dc603409e78294b918cbb7266981
Yifan Zhang <zhangyif21@mails.tsinghua.edu.cn>
2024-01-27 22:44:25 +0800

    minor

--- template-gsm/gsm-0005-1.py
+++ template-gsm/gsm-0005-1.py
@@ -125,7 +125,7 @@ NUM_PROBLEMS = args.num_problems
 if __name__ == "__main__":
     os.makedirs('./output', exist_ok=True)
     # output jsonl file
-    with open(f'./output/gsm-6-1--NUM{NUM_PROBLEMS}.jsonl', 'w') as f:
+    with open(f'./output/gsm-0005-1--NUM{NUM_PROBLEMS}.jsonl', 'w') as f:
         for i in range(NUM_PROBLEMS):
             problem, solution_code, result, solution_wocode = generate_problem_and_solution_code()
             # Write problem to file
--- template-gsm/gsm-0010-1.py
+++ template-gsm/gsm-0010-1.py
@@ -110,7 +110,7 @@ NUM_PROBLEMS = args.num_problems
 if __name__ == "__main__":
     os.makedirs('./output', exist_ok=True) 
     # output jsonl file
-    with open(f'./output/gsm-11-1--NUM{NUM_PROBLEMS}.jsonl', 'w') as f:
+    with open(f'./output/gsm-0010-1--NUM{NUM_PROBLEMS}.jsonl', 'w') as f:
         for i in range(NUM_PROBLEMS):
             problem, solution_code, result, solution_wocode = generate_problem_and_solution_code()
             # Write problem to file
